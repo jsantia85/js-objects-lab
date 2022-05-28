d4e3413815e9416a24ab5f9f796c6a807e27348f
JASantia85 <JuanASantiago1058@gmail.com>
2022-05-28 16:40:47 -0400

    completes second part to play function

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
-//ways to create objects--
-//object literal notation const game = {} 
-
-
 const game = {
   title: 'Guess the Number!',
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+
   //initialize empty array.
   prevGuesses: [],
 
+  //play section: 
   play: function() {
+    this.setRange()
+
+    //Get the secretNum from the range bellow. Note on this, when not attached to anything this will refer the get global object which is the window in a browser 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this' I assume that this. in the code below refers to secretNum: null above in line 9
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-      console.log(this.secretNum)
+      console.log(this.secretNum);
+
+      //For this situation the game loop should run at least once, thus do...while loop works for this situation 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/do...while'
+    do {
+      //first command: Get the player's valid guess and push it into the prevGuesses array.
+      this.prevGuesses.push(this.getGuess());
+
+      //second command: call the resetRange method, and reset the range after a guess has been made
+      this.resetRange();
+
+      //third command: call the render mothod and render the result of the player's guess
+      this.render();
+
+      //fourth command: if the player's last gues was not the secretNum, do it all again
+    } while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum);
   },
 
-  
+
   }
