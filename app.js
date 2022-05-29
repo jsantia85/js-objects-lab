dc2cb2be1d227ea14db80141e6c7d75f7d4ad24a
JASantia85 <JuanASantiago1058@gmail.com>
2022-05-29 11:58:29 -0400

    fixes syntax and gets game to work

--- app.js
+++ app.js
@@ -1,42 +1,31 @@
 const game = {
-  title: 'Guess the Number!',
+  title: "Guess the Number!",
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
   //initialize empty array.
   prevGuesses: [],
-
   //starts play function: 
   play: function() {
     this.setRange()
-
     //Get the secretNum from the range bellow. Note on this, when not attached to anything this will refer the get global object which is the window in a browser 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this' I assume that this. in the code below refers to secretNum: null above in line 9
-    this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-      console.log(this.secretNum);
-
+    this.secretNum = 
+      Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
       //For this situation the game loop should run at least once, thus do...while loop works for this situation 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/do...while'
     do {
-
       //first command: Get the player's valid guess and push it into the prevGuesses array.
       this.prevGuesses.push(this.getGuess());
-
       //second command: call the resetRange method, and reset the range after a guess has been made
       this.resetRange();
-
       //third command: call the render method and render the result of the player's guess
       this.render();
-
       //fourth command: if the player's last guess was not the secretNum, do it all again
     } while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum);
   },
-
   //starts of getGuess: function
-
   getGuess: function () {
     //initialize a guess variable
     let guess; 
-
     do {
       //assign guess as an integer value parsed from the player's input to the prompt
       guess = parseInt(
@@ -52,9 +41,8 @@ const game = {
     //after loop done once, return the guess
     return guess; 
   },
-
   //starts setRange: function
-  setRange : function() {
+  setRange: function() {
     do {
       //assign this.smallestNum as an integer value parsed from the player's input to the prompt
       this.smallestNum = parseInt(
@@ -67,7 +55,7 @@ const game = {
       //assign this.highestNum as an integer value parsed from the player's input to the prompt
       this.biggestNum = parseInt(
         prompt(
-          `Enter a number that is ${this.smallestNum +2} or more. This will be the high end of the range.`
+          `Enter a number that is ${this.smallestNum + 2} or more. This will be the high end of the range.`
         )
       );
     } while (//while this.higestNum is not a number or while this.highestNum isn't larger than this.smallestNum + 2 (to allow space for this.secretNum to exist)
@@ -86,29 +74,25 @@ const game = {
       this.smallestNum = this.prevGuesses[this.prevGuesses.length - 1]
     }
   },
-  
   //starts render: function
-  render: function(){
+  render: function() {
     //initalize a msg varible
     let msg;
-
     //if the player has guessed the secretNum
     if (this.prevGuesses[this.prevGuesses.length - 1] === this.secretNum) {
       //assign the victory message to the msg varible
-      msg = `Congrats! you guessed the number in ${this.prevGuesses.length} ${this.prevGuesses.length > 1 ? 'guesses' : 'guess'
+      msg = `Congrats! you guessed the number in ${this.prevGuesses.length} ${this.prevGuesses.length > 1 ? "guesses" : "guess"
     }!`;
     } else {
       //if not, assign the too high/low message to the msg varible 
       msg = `Your guess is too ${this.prevGuesses[this.prevGuesses.length - 1] > this.secretNum
-      ? 'high'
-      : 'low'
-    }/nPrevious guesses: ${this.prevGuesses.join(',')}`;
+      ? "high"
+      : "low"
+    }\nPrevious guesses: ${this.prevGuesses.join(", ")}`;
     }
-  }
-
-
-  }
-
-  //call game.play and start the game
-
-  game.place();
+    //show the message in an alert dialog box
+    alert(msg);
+  },
+};
+//call game.play and start the game
+game.play();
